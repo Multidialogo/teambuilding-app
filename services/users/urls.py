7f9cacb70e2324aaa2e6e9175b67c3de83237e0c
Emidio Cruciani <cruciani@multidialogo.it>
2022-01-05 23:17:07 +0100

    FEAT/TEAM/010/PRE01- Users

--- services/users/urls.py
+++ services/users/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 
 urlpatterns = [
-    path('logout/', views.logout_view, name='logout'),
-    path('signup/', views.signup, name='signup'),
-    path('signin/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('auth/logout/', views.logout_view, name='logout'),
+    path('auth/signup/', views.signup, name='signup'),
+    path('auth/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('profile/', views.profile, name='profile'),
     path('profile/change_nickname', views.change_nickname, name='change_nickname'),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('auth/activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
