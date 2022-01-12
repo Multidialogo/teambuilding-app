7a62d58dc5e8d733c6f456b5d6170bec032f3c66
Emidio Cruciani <cruciani@multidialogo.it>
2022-01-12 12:09:18 +0100

    TB/TAPU/PWD/BE/01-Funzionalità recupero password

--- teambuilding/accounts/tasks.py
+++ teambuilding/accounts/tasks.py
@@ -6,20 +6,57 @@ from django.utils.http import urlsafe_base64_encode
 from django.utils.translation import gettext
 
 
-def send_activation_mail_to_user(user, site_domain):
-    if not user.is_superuser:
-        base64_user_id = urlsafe_base64_encode(force_bytes(user.pk))
-        token = default_token_generator.make_token(user)
-        activation_path = reverse('activate', kwargs={'uid_64': base64_user_id, 'token': token})
-        activation_link = "https://%s%s" % (site_domain, activation_path)
+def send_activation_mail_to_user(account, site_domain):
+    if not account.is_superuser:
+        base64_user_id = urlsafe_base64_encode(force_bytes(account.pk))
+        token = default_token_generator.make_token(account)
+
+        activation_url_args = {'uidb64': base64_user_id, 'token': token}
+        activation_url_path = reverse('activate', kwargs=activation_url_args)
+        activation_link = "https://%s%s" % (site_domain, activation_url_path)
+
+        mail_subject = gettext('Taste & Purchase account activation')
         message = gettext(
             "Hi %(nickname)s,\n"
             "Please click on the link to confirm your registration, %(link)s\n"
             "If you think it's not you, then just ignore this email."
-        ) % (
-            {'nickname': user.nickname, 'link': activation_link}
-        )
+        ) % ({
+            'nickname': account.nickname,
+            'link': activation_link
+        })
 
-        mail_subject = gettext('Taste & Purchase account activation')
-        email = EmailMessage(mail_subject, message, to=(user.email,))
+        email = EmailMessage(mail_subject, message, to=(account.email,))
         email.send()
+
+
+def send_reset_password_mail_to_user(account, site_domain):
+    base64_user_id = urlsafe_base64_encode(force_bytes(account.pk))
+    token = default_token_generator.make_token(account)
+
+    reset_url_args = {'uidb64': base64_user_id, 'token': token}
+    reset_url_path = reverse('password-reset-confirm', kwargs=reset_url_args)
+    reset_link = "https://%s%s" % (site_domain, reset_url_path)
+    website_link = reverse('home')
+
+    mail_subject = gettext('Password Reset Requested')
+    message = gettext(
+        "Hello,\n"
+        "We've received a request to reset the password for your account for this email address.\n"
+        "To initiate the password reset process for your account, click the link below.\n"
+        "\n"
+        "%(reset-link)s\n"
+        "This link can only be used once. If you need to reset your password again,\n"
+        "please visit %(website-link)s and request another reset.\n"
+        "\n"
+        "If you did not make this request, you can simply ignore this email.\n"
+        "\n"
+        "Sincerely,\n"
+        "The Taste & Purchase Team\n"
+    ) % ({
+        'nickname': account.nickname,
+        'reset-link': reset_link,
+        'website-link': website_link
+    })
+
+    email = EmailMessage(mail_subject, message, to=(account.email,))
+    email.send()
--- teambuilding/accounts/views.py
+++ teambuilding/accounts/views.py
@@ -3,6 +3,7 @@ from copy import copy
 from django.conf import settings
 from django.contrib.auth import logout as logout_request, get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.db import transaction
@@ -11,7 +12,8 @@ from django.utils.http import urlsafe_base64_decode
 from django.utils.translation import gettext
 
 from .forms import RegistrationForm
-from .tasks import send_activation_mail_to_user
+from .models import UserAccount
+from .tasks import send_activation_mail_to_user, send_reset_password_mail_to_user
 
 
 @login_required
@@ -47,9 +49,9 @@ def signup(request):
     return render(request, 'teambuilding/account/signup.html', context)
 
 
-def activate(request, uid_64, token):
+def activate(request, uidb64, token):
     try:
-        user_id = urlsafe_base64_decode(uid_64).decode()
+        user_id = urlsafe_base64_decode(uidb64).decode()
         user = get_user_model().objects.get(pk=user_id)
     except(TypeError, ValueError, OverflowError, settings.AUTH_USER_MODEL.DoesNotExist):
         user = None
@@ -65,3 +67,23 @@ def activate(request, uid_64, token):
 
     context = {'message': message}
     return render(request, 'teambuilding/account/activate.html', context)
+
+
+def password_reset(request):
+    if request.method == 'POST':
+        post_args = copy(request.POST)
+        form = PasswordResetForm(post_args)
+
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            account = UserAccount.objects.filter(email__exact=email).first()
+
+            if account:
+                site_domain = get_current_site(request).domain
+                send_reset_password_mail_to_user(account, site_domain)
+                return redirect('password-reset-done')
+    else:
+        form = PasswordResetForm()
+
+    context = {'form': form}
+    return render(request, 'teambuilding/account/password_reset.html', context)
