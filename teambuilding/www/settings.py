3d72eeea8bc5d7317cc44eb55e24f5ce76c2b116
Emidio Cruciani <cruciani@multidialogo.it>
2022-01-18 11:58:38 +0100

    TB/TAPU/DEPLOY/04-Rimossa default secret key dai settings

--- teambuilding/www/settings.py
+++ teambuilding/www/settings.py
@@ -23,7 +23,7 @@ PROJECT_DIR = Path(__file__).resolve().parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', "n3r4zq2uum(ouciq8od=$q1!$zxl_&z#fa*el(r!kp+wnus^=c")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(strtobool(os.getenv('DEBUG', False)))
